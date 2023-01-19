6f88d1de7d95023746a7511b0305866bd4345706
halodub <100950452+halodub@users.noreply.github.com>
2023-01-19 07:47:15 -0600

    Update background.sh

--- thinkpad/background.sh
+++ thinkpad/background.sh
@@ -1,5 +1,3 @@
-urxvt -bl -geometry 235x86-0+0 -e sh -c "sleep 1;wmctrl -r sh -b add,skip_taskbar;wmctrl -r sh -t -1; btop"
+urxvt -bl -geometry 100x86+0+0 -e ksh -li &
 sleep 3
-urxvt -bl -geometry 85x86+0+0 &
-sleep 3
-wmctrl -r urxvt -b add,skip_taskbar
+wmctrl -r ksh -b add,skip_taskbar
